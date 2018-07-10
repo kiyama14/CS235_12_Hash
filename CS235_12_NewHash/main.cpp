6003040436847a9342b1c4e64af50500af6a9862
kiyama14 <kiy11001@byui.edu>
2018-07-10 12:41:50 -0600

    Initial Commit

---
+++ CS235_12_NewHash/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS235_12_NewHash
+//
+//  Created by Mauricio Takashi Kiyama on 7/10/18.
+//  Copyright © 2018 Marcio Kazuo Kiyama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
